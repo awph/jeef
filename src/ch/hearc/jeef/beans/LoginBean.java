c20d1c0f0be124f7625bd985a07ca8dcc187cd15
Alexandre Perez <alexandre.perez92@gmail.com>
2014-03-17 22:23:11 +0100

    Use named instead managedbean for login

--- src/ch/hearc/jeef/beans/LoginBean.java
+++ src/ch/hearc/jeef/beans/LoginBean.java
@@ -5,25 +5,24 @@
  */
 package ch.hearc.jeef.beans;
 
-import ch.hearc.jeef.controller.UserController;
 import ch.hearc.jeef.entities.User;
 import ch.hearc.jeef.facade.UserFacade;
 import java.io.IOException;
 import java.io.Serializable;
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
+import javax.enterprise.context.SessionScoped;
 import javax.faces.application.FacesMessage;
-import javax.faces.bean.ManagedBean;
-import javax.faces.bean.SessionScoped;
 import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
+import javax.inject.Named;
 import javax.servlet.RequestDispatcher;
 
 /**
  *
  * @author Alexandre
  */
-@ManagedBean
+@Named("loginBean")
 @SessionScoped
 public class LoginBean implements Serializable {
 
