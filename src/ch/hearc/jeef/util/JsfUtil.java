ba79d556434f51308869ae86bc2186d72f57f7df
Alexandre Perez <alexandre.perez92@gmail.com>
2014-04-17 14:19:04 +0200

    Fixed success message position

--- src/ch/hearc/jeef/util/JsfUtil.java
+++ src/ch/hearc/jeef/util/JsfUtil.java
@@ -45,7 +45,7 @@ public class JsfUtil {
 
     public static void addSuccessMessage(String msg) {
         FacesMessage facesMsg = new FacesMessage(FacesMessage.SEVERITY_INFO, msg, msg);
-        FacesContext.getCurrentInstance().addMessage("successInfo", facesMsg);
+        FacesContext.getCurrentInstance().addMessage(null, facesMsg);
     }
 
     public static String getRequestParameter(String key) {
